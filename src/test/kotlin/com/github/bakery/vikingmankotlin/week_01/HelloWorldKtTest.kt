533eeb2a76a3971b8886be7614af6889c5e52395
hotire <gngh0101@gmail.com>
2020-02-15 00:32:08 +0900

    Add 0주차 수업 및 과제

---
+++ src/test/kotlin/com/github/bakery/vikingmankotlin/week_01/HelloWorldKtTest.kt
@@ -0,0 +1,28 @@
+package com.github.bakery.vikingmankotlin.week_01
+
+import org.junit.jupiter.api.Test
+
+
+internal class HelloWorldKtTest {
+    /**
+     * hello 를 검증하세요..!!
+     */
+    @Test
+    fun hello() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    fun printHello() {
+        // given
+        val hello = "hello";
+
+        // 문자열 템플릿을 사용해 hello를 출력하세요.
+
+        // no assert
+    }
+}
