21990de532473bc4e079b037425390886080ccd3
hotire <gngh0101@gmail.com>
2020-02-19 01:34:51 +0900

    1주차 과제 추가

---
+++ src/test/kotlin/com/github/bakery/vikingmankotlin/week_01/BaseServiceTest.kt
@@ -0,0 +1,17 @@
+package com.github.bakery.vikingmankotlin.week_01
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertFailsWith
+
+internal class BaseServiceTest {
+    class WrapperException(e : Exception) : RuntimeException()
+
+    /**
+     * catch 해서 테스트 코드를 완성하세요..!!
+     */
+    @Test
+    fun catch() {
+        val service = BaseService()
+        assertFailsWith<WrapperException>{ service.service() }
+    }
+}
