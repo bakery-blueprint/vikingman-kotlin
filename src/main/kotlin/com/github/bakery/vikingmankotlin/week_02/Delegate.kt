ad5974ac8a51e22a2517339ee58e41de5883ec62
wenmoe <nacom1994@gmail.com>
2020-03-11 20:51:42 +0900

    2주차 과제 (1차)

--- src/main/kotlin/com/github/bakery/vikingmankotlin/week_02/Delegate.kt
+++ src/main/kotlin/com/github/bakery/vikingmankotlin/week_02/Delegate.kt
@@ -2,3 +2,11 @@ package com.github.bakery.vikingmankotlin.week_02
 
 // TODO: Delegate를 이용해 값을 넣을 때마다 그 값을 로깅해주는 MutableList를 만들어봅시다.
 
+class LoggingList<T>(
+        val innerSet : MutableCollection<T> = mutableListOf()
+) : MutableCollection<T> by innerSet {
+    override fun add(element: T): Boolean {
+        println("add $element")
+        return innerSet.add(element)
+    }
+}
--- src/main/kotlin/com/github/bakery/vikingmankotlin/week_02/ForToLambda.kt
+++ src/main/kotlin/com/github/bakery/vikingmankotlin/week_02/ForToLambda.kt
@@ -3,9 +3,6 @@ package com.github.bakery.vikingmankotlin.week_02
 // TODO: 람다를 사용하도록 변경해봅시다.
 fun forloop(targets: Iterable<String>) {
     var firstChars = ""
-    for (target in targets) {
-        if (target.isNotEmpty()) {
-            firstChars += target[0]
-        }
-    }
+    targets.filter { it.isNotEmpty() }
+            .map { firstChars += it[0]}
 }
