1992f12031c0a8ba06a578691522ae17a63043c7
hotire <gngh0101@gmail.com>
2020-03-24 00:23:11 +0900

    Add 5주차 리프렉션과 어노테이션

---
+++ src/main/kotlin/com/github/bakery/vikingmankotlin/week_05/MyDeprecated.java
@@ -0,0 +1,20 @@
+package com.github.bakery.vikingmankotlin.week_05;
+
+import kotlin.DeprecationLevel;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value={CONSTRUCTOR, FIELD, LOCAL_VARIABLE, METHOD, PACKAGE, PARAMETER, TYPE, MODULE})
+public @interface MyDeprecated {
+    String message();
+    ReplaceWith replaceWith() default @ReplaceWith(expression = "");
+    DeprecationLevel level() default DeprecationLevel.WARNING;
+}
