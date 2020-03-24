4a11840033ff5e7b016aff97b11421b7e340f2ef
wenmoe <nacom1994@gmail.com>
2020-03-24 19:56:40 +0900

    4주차 과제

---
+++ src/main/kotlin/com/github/bakery/vikingmankotlin/week_04/DeclarationSiteVariance.kt
@@ -0,0 +1,10 @@
+package com.github.bakery.vikingmankotlin.week_04
+
+interface Source<T> {
+    fun nextT(): T
+}
+
+fun demo(strs: Source<String>) {
+    // 9장의 내용을 바탕으로 해당 구문이 가능하도록 수정해봅시다
+    //val objects: Source<Any> = strs
+}
